8dd202a5ef7030bd5be494866030649bfa4b7f64
Major Hayden <major@mhtx.net>
2015-07-09 21:58:33 -0500

    More tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,3 +5,11 @@ class TestUtils(object):
     def test_get_ram(self):
         ram = utils.get_ram()
         assert ram > 0
+
+    def test_get_swap(self):
+        swap = utils.get_swap()
+        assert swap > 0
+
+    def test_get_uptime(self):
+        uptime = utils.get_uptime()
+        assert uptime > 0
