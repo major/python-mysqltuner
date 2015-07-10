9f9bbfbeb67c460c94ba82fde6b2dde730864197
Major Hayden <major@mhtx.net>
2015-07-09 22:00:57 -0500

    Converting to click

--- mysqltuner/executable.py
+++ mysqltuner/executable.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import click
 import psutil
 import pymysql
 from pprint import pprint
@@ -7,33 +8,6 @@ from pprint import pprint
 from . import mysqltuner
 
 
+@click.command()
 def run_mysqltuner():
-
-  m = mysqltuner.MysqlTuner()
-
-  # db_creds = {
-  #   'host': 'localhost',
-  #   'user': 'root',
-  # }
-
-  # conn = pymysql.connect(**db_creds)
-
-  # cur = conn.cursor()
-
-  # queries = [
-  #   "SHOW /*!50000 GLOBAL */ VARIABLES",
-  #   "SHOW /*!50000 GLOBAL */ STATUS"
-  # ]
-
-  # for query in queries:
-  #   cur.execute(query)
-
-  #   for row in cur:
-  #     print(row)
-
-
-  # cur.close()
-  # conn.close()
-
-  # pprint(psutil.virtual_memory())
-  # pprint(psutil.swap_memory())
+  click.echo("Running MySQLTuner")
