3057f560b6710e965861c1495323253e487114e4
Major Hayden <major@mhtx.net>
2015-07-09 21:53:51 -0500

    Fixing mysqltuner class

--- mysqltuner/mysqltuner.py
+++ mysqltuner/mysqltuner.py
@@ -9,16 +9,3 @@ class MysqlTuner(object):
 
   def __init__(self):
     pass
-
-    def get_ram():
-      return psutil.virtual_memory().total
-
-    def get_swap():
-      return psutil.swap_memory().total
-
-    def get_uptime():
-      return int(time.time())-psutil.boot_time()
-
-    self.data.ram = get_ram()
-    self.data.swap = get_swap()
-    self.data.uptime = get_uptime()
